9d9647a00be9dfe570a72a10ec79d2792392fe9f
yashwant <yashtejwani00@gmail.com>
2020-10-31 01:24:02 +0530

    made code of Cycle Detection (need to be tested by Srishti)
    FILE : cd.java

---
+++ app/src/main/java/com/example/graf/cd.java
@@ -0,0 +1,46 @@
+package com.example.graf;
+
+import android.graphics.Color;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class cd extends Result{
+    public void result(){
+        finalResult.setData(graph , prevheight , type , start);
+        Map<String , Boolean> visited = new HashMap<>();
+        Map<String , Boolean> recStack = new HashMap<>();
+        for(String i : graph.getNodeList().keySet()){
+            visited.put(i,false);
+            recStack.put(i,false);
+        }
+        for(String i : graph.getNodeList().keySet()){
+            if(isCyclic(i,visited,recStack)){
+                //graph has a cycle cyclic
+                graph.getNodeList().get(i).updateHex(Color.YELLOW);
+                return;
+            }
+        }
+        Toast.makeText(this, "No Cycle Found", Toast.LENGTH_LONG).show();
+    }
+    public Boolean isCyclic(String i,Map<String , Boolean> visited ,Map<String , Boolean> recStack){
+        if(recStack.get(i)) return true;
+        if(visited.get(i)) return false;
+        visited.put(i,true);
+        recStack.put(i,true);
+        ArrayList<String> children = graph.getAdjacencylist().get(i);
+        for(String c : children){
+            if(isCyclic(c,visited,recStack)) {
+                graph.getNodeList().get(c).updateHex(Color.YELLOW);
+                graph.getEdgeList().get(i+c).updateHex(Color.YELLOW);
+                if(type==0)
+                    graph.getEdgeList().get(c+i).updateHex(Color.YELLOW);
+                return true;
+            }
+        }
+        recStack.put(i,false);
+        return  false;
+    }
+}
