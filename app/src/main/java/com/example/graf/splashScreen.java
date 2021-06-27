8bba2eb9decea2e0737d763af8a9f50c0a1ca8e4
Srishti Naruka <srishtinaruka13@gmail.com>
2021-06-27 23:36:07 +0530

    Changes done by Srishti

---
+++ app/src/main/java/com/example/graf/splashScreen.java
@@ -0,0 +1,28 @@
+package com.example.graf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashScreen extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                    Intent intent = new Intent(splashScreen.this , MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+            }
+        },2000);
+    }
+}
